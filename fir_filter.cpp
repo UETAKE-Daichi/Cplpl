c7da4b677e2186aa89a2834ed62a92255fdf6297
daichi_uetake <uetake@virgo.is.utsunomiya-u.ac.jp>
2015-03-03 20:17:02 +0900

    Create fir_filter.cpp

---
+++ fir_filter.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	char input_filename[] = "input.txt";
+	char output_filename[] = "output.txt";
+	
+	FILE *input_fp;
+	FILE *output_fp;
+
+	char input_data[100]; //入力文字列データ
+	int data[100]; //入力整数データ用
+	int out[100] ;
+	int i = 0;
+	int count=0;
+
+	for(i = 0 ; i < 100;i++){
+		data[i]=0;
+		out[i]=0;
+	}
+
+
+	if ((input_fp = fopen(input_filename, "r")) == NULL)
+    {
+        printf("inputファイルが存在しません。プログラムを終了します。\n");
+        exit(1);
+    }
+	
+	
+	if ((output_fp = fopen(output_filename, "w")) == NULL)
+    {
+        printf("outputファイルが存在しません。プログラムを終了します。\n");
+        exit(1);
+    }
+
+	count = 9;
+	//入力読み込み
+	while(fgets(input_data, 100, input_fp ) != NULL ){
+		data[count] =  atoi(input_data);
+		count++;
+		
+	}
+	
+
+	//計算
+	for(i = 9; i < 28; i++ ){
+		out[i] = (480*data[i] +
+				  268*data[i-1]+
+				  145*data[i-2]+
+				   71*data[i-3]+
+				   35*data[i-4]+
+				   13*data[i-5]+
+				    6*data[i-6]+
+					3*data[i-7]+
+					2*data[i-8]+
+				    1*data[i-9])/1024;
+		printf("%d \n",out[i]);
+
+		//fputs(out[i],output_fp);
+	}
+}
